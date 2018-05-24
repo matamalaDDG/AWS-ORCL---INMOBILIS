9334d291d945aef9011f7ffc80947fe693749a8c
Sergio matamala <Smatamala92@gmail.com>
2018-05-24 12:27:23 -0400

    Base Proyecto

---
+++ src/main/java/com/api/apisigi/ApisigiApplication.java
@@ -0,0 +1,12 @@
+package com.api.apisigi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisigiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApisigiApplication.class, args);
+    }
+}
