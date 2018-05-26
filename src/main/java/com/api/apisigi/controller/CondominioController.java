7c95e932833207a1f12f222d7b9a3a403e2f21d1
Sergio matamala <Smatamala92@gmail.com>
2018-05-26 12:45:48 -0400

    Cambios varios

---
+++ src/main/java/com/api/apisigi/controller/CondominioController.java
@@ -0,0 +1,10 @@
+package com.api.apisigi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/condominio")
+public class CondominioController {
+
+}
--- src/main/java/com/api/apisigi/repository/IRCondominio.java
+++ src/main/java/com/api/apisigi/repository/IRCondominio.java
@@ -4,8 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.api.apisigi.entity.Condominio;
 
-import java.io.Serializable;
 
 @Repository("condominioRepo")
-public interface IRCondominio extends JpaRepository<Condominio, Serializable> {
+public interface IRCondominio extends JpaRepository<Condominio, String> {
+    public abstract
 }
