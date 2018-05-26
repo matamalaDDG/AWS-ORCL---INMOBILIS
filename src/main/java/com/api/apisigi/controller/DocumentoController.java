16622359370175ad04a385bdb8551cc3d53608d5
Sergio matamala <Smatamala92@gmail.com>
2018-05-26 18:00:34 -0400

    Documento controller finalizado

--- src/main/java/com/api/apisigi/controller/DocumentoController.java
+++ src/main/java/com/api/apisigi/controller/DocumentoController.java
@@ -6,6 +6,7 @@ import com.api.apisigi.repository.IRDocumento;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -27,16 +28,18 @@ public class DocumentoController {
     public List<Documento> getAll() {
         return documentorepo.findAll();
     }
+
     //TODO
     //    GET MAPPING: ById
     //    GET MAPPING: ByDescripcion
     //    GET MAPPING: ByDocumento
 
+    
     //    POST MAPPING
     @PostMapping("/ndocumento")
     @ResponseBody
     @JsonFormat
-    public Documento createRegion(@Valid @RequestBody Documento documento) {
+    public Documento createDocumento(@Valid @RequestBody Documento documento) {
         // logger.info("[creando region : ROUTE: /dregion/{regionId}.... Method: createRegion]");
         return documentorepo.save(documento);
     }
@@ -45,13 +48,27 @@ public class DocumentoController {
     @PutMapping("/udocumento/{documentoId}")
     @ResponseBody
     @JsonFormat
-    public Documento updatePost(@PathVariable String documentoId,
-                                @Valid @RequestBody Documento documentorequest) {
+    public Documento updateDocumento(@PathVariable String documentoId,
+                                     @Valid @RequestBody Documento documentorequest) {
         return documentorepo.findById(documentoId).map(documento -> {
             documento.setIdDocumento(documentorequest.getIdDocumento());
             documento.setDescripcion(documentorequest.getDescripcion());
             documento.setDocumento(documentorequest.getDocumento());
             return documentorepo.save(documento);
+        }).orElseThrow(() -> new ResourceNotFoundExcption("PostId " + documentoId + " not found"));
+    }
+
+    //    DELETE MAPPING
+    @DeleteMapping("/ddocumento/{documentoId}")
+    @ResponseBody
+    @JsonFormat
+    public ResponseEntity<?> deleteRegion(@PathVariable String documentoId) {
+        //    logger.info("[buscado region : /dregion/{regionId}.... Method: deletePost]");
+        return documentorepo.findById(documentoId).map(documento -> {
+            //        logger.info("[Eliminando region  region : /dregion/{regionId}.... Method: deletePost]");
+            documentorepo.delete(documento);
+            //         logger.info("[region eliminada: /dregion/{regionId}.... Method: deletePost .... state: success]");
+            return ResponseEntity.ok().build();
         }).orElseThrow(() -> new ResourceNotFoundExcption("PostId " + documentoId + " not found"));
     }
 
