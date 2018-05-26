786af8d2253289941321aad6f500751b2e875137
Sergio matamala <Smatamala92@gmail.com>
2018-05-26 13:09:06 -0400

    Condomionio controller

--- src/main/java/com/api/apisigi/controller/CondominioController.java
+++ src/main/java/com/api/apisigi/controller/CondominioController.java
@@ -1,10 +1,71 @@
 package com.api.apisigi.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.api.apisigi.entity.Condominio;
+import com.api.apisigi.exception.ResourceNotFoundExcption;
+import com.api.apisigi.repository.IRCondominio;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/condominio")
 public class CondominioController {
+
+
+    @Autowired
+    @Qualifier("condominioRepo")
+    private IRCondominio condomioniorepo;
+
+    //#REQUEST AREA
+    //#GETMAPPING
+    @GetMapping("/condominios")
+    @ResponseBody
+    @JsonFormat
+    public List<Condominio> getBodegas() {
+        return condomioniorepo.findAll();
+    }
+
+    //#POSTMAPPING
+    @PostMapping("/ncondominio")
+    @ResponseBody
+    @JsonFormat
+    public Condominio createEdificio(@Valid @RequestBody Condominio condominio) {
+        //logger.info("[creando region : ROUTE: /dregion/{regionId}.... Method: createRegion]");
+        return condomioniorepo.save(condominio);
+    }
+
+    //#UPDATEMAPPING
+    @PutMapping("/econdominio/{condominioId}")
+    @ResponseBody
+    @JsonFormat
+    public Condominio updateEdificio(@PathVariable String condominioId,
+                                     @Valid @RequestBody Condominio condomioniorequest) {
+        return condomioniorepo.findById(condominioId).map(condominio -> {
+            condominio.setIdCondominio(condomioniorequest.getIdCondominio());
+            condominio.setNombre(condomioniorequest.getNombre());
+            condominio.setNumeroCond(condomioniorequest.getNumeroCond());
+            return condomioniorepo.save(condominio);
+        }).orElseThrow(() -> new ResourceNotFoundExcption("PostId " + condominioId + " not found"));
+    }
+
+    //#DELETEMAPPING
+    @DeleteMapping("/dcondominio/{condominioId}")
+    @ResponseBody
+    @JsonFormat
+    public ResponseEntity<?> deleteRegion(@PathVariable String condominioId) {
+        //logger.info("[buscado region : /dregion/{regionId}.... Method: deletePost]");
+        return condomioniorepo.findById(condominioId).map(condominio -> {
+            //   logger.info("[Eliminando region  region : /dregion/{regionId}.... Method: deletePost]");
+            condomioniorepo.delete(condominio);
+            //   logger.info("[region eliminada: /dregion/{regionId}.... Method: deletePost .... state: success]");
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundExcption("PostId " + condominioId + " not found"));
+    }
+
 
 }
--- src/main/java/com/api/apisigi/repository/IRCondominio.java
+++ src/main/java/com/api/apisigi/repository/IRCondominio.java
@@ -1,5 +1,7 @@
 package com.api.apisigi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.api.apisigi.entity.Condominio;
@@ -7,5 +9,6 @@ import com.api.apisigi.entity.Condominio;
 
 @Repository("condominioRepo")
 public interface IRCondominio extends JpaRepository<Condominio, String> {
-    public abstract
+    public abstract Page<Condominio> findByIdCondominio(String id, Pageable pageable);
+    public abstract Page<Condominio> findAllB();
 }
